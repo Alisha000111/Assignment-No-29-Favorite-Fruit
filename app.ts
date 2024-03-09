1ab9684f65a57339fd87a55874b110a3356cdb14
Alisha M Irfan <160208713+Alisha000111@users.noreply.github.com>
2024-03-10 01:26:42 +0500

    Add files via upload

---
+++ app.ts
@@ -0,0 +1,17 @@
+// you really like bananas
+let favourite_fruits : string [] = ["mango","apple","blue berrys","kivi","peach"]
+if (favourite_fruits.includes("mango")) {
+console.log("mango")
+}
+if (favourite_fruits.includes("apple")) {
+    console.log("apple")
+}
+ if (favourite_fruits.includes("blue berrys")) {
+  console.log("blue berrys")
+    }
+   if (favourite_fruits.includes("kivi")) {
+     console.log("you reaaly like bananas")
+    }
+    if (favourite_fruits.includes("peach")) {
+        console.log("you reaaly like bananas")
+    }
